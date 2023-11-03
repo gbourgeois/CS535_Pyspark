2c9fc1b8c55b5edaae8204dca9f8a81fe48c92d2
amit <ajain@boisestate.edu>
2023-11-02 22:36:33 -0600

    cleanup

--- Cross_Correlation/cross-correlation.py
+++ Cross_Correlation/cross-correlation.py
@@ -1,54 +1,30 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
-from pyspark import SparkContext, SparkConf
-conf = SparkConf().setAppName('Homework').setMaster('local[*]')
-sc = SparkContext.getOrCreate(conf=conf)
-
-
-# In[2]:
-
+#!/usr/bin/env python3
 
 import os
-
-
-# In[3]:
-
-
+import sys
 import itertools
-path = "/Users/bourgeoisgadjagb/Documents/CS535_Spring2022/Cross-Correlation/data_2.txt"
-rdd = sc.wholeTextFiles(path)
-
-
-# In[4]:
-
-
 from itertools import combinations
 
+if (len(sys.argv) != 2):
+    print("Usage: cross-correlation.py <input folder>")
+    sys.exit(1)
 
-# In[5]:
+from pyspark import SparkContext, SparkConf
+conf = SparkConf().setAppName('Cross Correlation')
+sc = SparkContext.getOrCreate(conf=conf)
 
+rdd = sc.wholeTextFiles(sys.argv[1])
 
 rdd1 = rdd.flatMap(lambda file: [d.split() for d in file[1].split('\n') if d!=''])
 #rdd2 = rdd1.map(lambda file: (x,y) for x,y in file )
 rdd1.collect()
-rdd.take(1)
-
-
-# In[12]:
-
 
 rdd2 = rdd1.map(lambda x: list(combinations(x, 2))).flatMap(lambda x: x)
 rdd3 = rdd2.map(lambda x: (x,1)).groupByKey().mapValues(lambda vals:len(vals))
 rdd3.sortBy(lambda x: x[1], ascending = False)
-rdd3.collect()
 
-
-# In[ ]:
-
-
+result = rdd3.collect()
+for x in result:
+    print(x)
 
 
--- Pagerank/pagerank.py
+++ Pagerank/pagerank.py
@@ -1,53 +1,32 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # coding: utf-8
 
-# In[2]:
-
-
-#!pip install pyspark
 import os
+import itertools
+import sys
 from pyspark import SparkContext, SparkConf
-conf = SparkConf().setAppName('Homework').setMaster('local[*]')
+
+if (len(sys.argv) != 2):
+    print("Usage: case-analysis.py <input folder>")
+    sys.exit(1)
+
+conf = SparkConf().setAppName('pagerank')
 sc = SparkContext.getOrCreate(conf=conf)
 
-
-# In[98]:
-
-
-import itertools
-path = "/Users/bourgeoisgadjagb/Documents/CS535_Spring2022/Pagerank/pagerank_data_5.txt"
-rdd = sc.wholeTextFiles(path)
+rdd = sc.wholeTextFiles(sys.argv[1])
 rdd.collect()
 
-
-# In[99]:
-
-
 rdd2 = rdd.flatMap(lambda x: [d.split() for d in x[1].split('\n') if d!=''])
-#rdd2 = rdd2
-#.distinct()
 rdd3 = rdd2.groupByKey().mapValues(list)
-rdd3.collect()
-
-
-# In[100]:
-
 
 rdd4 = rdd3.map(lambda x: (x[0], 1.0))
-rdd4.collect()
-
-
-# In[101]:
-
 
 max_iter = 12
 for i in range(5):
     update_cont = rdd3.join(rdd4).flatMap(lambda x:[(b, x[1][1]/len(x[1][0])) for b in x[1][0]])    .groupByKey().mapValues(sum)
     rdd4 = update_cont
 
-
-# In[102]:
-
-
-rdd4.collect()
+result = rdd4.collect()
+for x in result:
+    print(x)
 
